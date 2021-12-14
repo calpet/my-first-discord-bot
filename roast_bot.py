6ffe8d396421f31be1ee649d993fdcdf3ae3624c
Calin Peters <calinpeters98@hotmail.nl>
2021-12-14 23:56:39 +0100

    Initial commit

--- roast_bot.py
+++ roast_bot.py
@@ -10,8 +10,5 @@ class RoastBotClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}.'.format(self.user))
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-
 client = RoastBotClient()
 client.run(TOKEN)
