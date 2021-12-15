d6ce9ad32be06245dcbea93fe4ad0ef286cc77ba
Calin Peters <calinpeters98@hotmail.nl>
2021-12-16 00:38:45 +0100

    Roast functionality fully works now.

--- roast_bot.py
+++ roast_bot.py
@@ -2,13 +2,15 @@ import discord
 import os
 import requests as req
 import json
+import re
+import string
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
-def get_roast():
-    res = req.get('https://insult.mattbas.org/api/en/insult.json?who=Calin')
+def get_roast(name):
+    res = req.get(os.getenv('API_URL') + name)
     json_data = json.loads(res.text)
     return json_data['insult']
 
@@ -22,10 +24,14 @@ async def on_message(message):
     if message.author == client.user:
         return
         
-    if message.content == '!roast':
-        roast = get_roast()
-        await message.channel.send(roast)
+    if message.content.startswith('!roast'):
+        # Spaghetti code in order to not get errors when I pass a discord UID to an API request.
+        pattern = r'[' + string.punctuation + ']'
+        uid = message.author.mention
+        stripped_uid = re.sub(pattern, '', uid)
+        roast = get_roast(stripped_uid)
+        roast = roast.replace(stripped_uid, uid)
 
-        #await message.channel.send('This is a roast.')
+        await message.channel.send(roast)
 
 client.run(TOKEN)
